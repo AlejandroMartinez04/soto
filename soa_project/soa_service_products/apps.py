39eed29a27ecc3fc96d4fbe5ca5d555a4f332d30
DiegoM <diegomartinez1101@gmail.com>
2023-10-24 23:27:30 -0500

    Second commit

---
+++ soa_project/soa_service_products/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SoaServiceProductsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'soa_service_products'
---
+++ soa_project/soa_service_users/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SoaServiceUsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'soa_service_users'
